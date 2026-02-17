efe6477160f5378460897b5dc4238691fb7c9890
Santosh Patel <santoshpatelvns5@gmail.com>
2026-02-17 21:24:24 +0530

    Account Model

---
+++ Models/Account.Models.js
@@ -0,0 +1,27 @@
+import mongoose, { mongo } from "mongoose";
+
+const AccountSchema=mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:[true,"Acccout  musst with Associated  with User"],
+        index:true
+    },
+    status:{
+        enum:{
+            values:["Active","Frozen","Closed"],
+            message :"Status can be Eigther Active Frozen or Closed"
+        },
+        type:String
+    },
+    currency:{
+        type:String,
+        required:[true,"Currency is Required for crreating an account"],
+        default :"INR"
+    },
+
+},{timestamps:true})
+
+AccountSchema.index({user:1,status:1});
+
+export const AccountModel=mongoose.models("Account",AccountSchema);
